969298cf71e2957379699228250d82497cce6f03
naitro2010 <108594252+naitro2010@users.noreply.github.com>
2026-09-22 03:22:35 -0700

    add version 6

--- ports/commonlibsse-gg/portfile.cmake
+++ ports/commonlibsse-gg/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_github(
     HEAD_REF main
 )
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH/extern/openvr
+    OUT_SOURCE_PATH "${SOURCE_PATH}/extern/openvr"
     REPO ValveSoftware/openvr
     REF v1.0.15
     SHA512 22ad52a659e1d2e4b52832400ac5c6766d1657cb81dfb9868bdc253120c661d41eeea68991eac44af9179d2bf6a346f038f1c444278a98b55b6b738af90ba1b5
